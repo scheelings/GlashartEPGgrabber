951a99e572aff38fa7004960583c85216915bb2f
Jan Saris <jan.saris@gmail.com>
2015-05-06 07:38:17 +0200

    Add linux create script

---
+++ Linux/CreateExecutable.sh
@@ -0,0 +1,2 @@
+mkbundle -o GlashartEPGgrabber GlashartEPGgrabber.exe GlashartLibrary.dll HtmlAgilityPack.dll log4net.dll Newtonsoft.Json.dll ICSharpCode.SharpZipLib.Portable.dll --deps --static --machine-config machine.config
+
